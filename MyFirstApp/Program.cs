715aea5d3853e27f921ed01bed6b6899e8c6df5d
1AlexDev <157933564+1AlexDev@users.noreply.github.com>
2024-11-02 23:27:15 +0000

    Updates and File Changes!

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What do you want to use? (*, +, -, /): ");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (operation == "*") {
+                double result = num1 * num2;
+                Console.WriteLine($"The result is {result}");
+            } else if (operation == "+") {
+                double result = num1 + num2;
+                Console.WriteLine($"The result is {result}");
+            } else if (operation == "-") {
+                double result = num1 - num2;
+                Console.WriteLine($"The result is {result}");
+            } else if (operation == "/") {
+                double result = num1 / num2;
+                Console.WriteLine($"The result is {result}");
+            } else {
+                Console.WriteLine("Invalid operation. Please enter a valid operator.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
